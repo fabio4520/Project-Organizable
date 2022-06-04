5369d68d98138865250dbd39d84fe9591daefb7a
fabio4520 <fabioleofc@gmail.com>
2022-06-04 00:58:56 -0500

    ADD: listenRestore to closedPage

--- assets/scripts/views/closed.js
+++ assets/scripts/views/closed.js
@@ -1,5 +1,5 @@
 import DOMHandler from "../dom-handler.js";
-import { deleteBoard } from "../services/boards-services.js";
+import { deleteBoard, editBoard } from "../services/boards-services.js";
 import STORE from "../store.js";
 import { renderBoard } from "./boards.js";
 
@@ -42,36 +42,34 @@ function listenDelete() {
   });
 }
 
-// function listenRestore() {
-//   const container = document.querySelector(".js-notes");
-//   if (!container) return;
-//   container.addEventListener("click", (e) => {
-//     const restoreArrows = container.querySelectorAll(".restore-trigger");
-//     restoreArrows.forEach((restoreArrow) => {
-//       if (restoreArrow === e.target) {
-//         e.preventDefault();
-//         const parentNote = restoreArrow.closest(".note");
-//         parentNote.classList.add("goBack");
-//         parentNote.addEventListener("animationend", (e) => {
-//           loadContent();
-//         });
-//         STORE.notes = STORE.notes.map((note) => {
-//           if (note.id === parentNote.dataset.id) {
-//             return { ...note, deleted: false };
-//           }
-//           return note;
-//         });
-//       }
-//     });
-//   });
-// }
+function listenRestore() {
+  const container = document.querySelector(".js-boards");
+  if (!container) return;
+  container.addEventListener("click", (e) => {
+    console.log(e);
+    const restoreArrows = container.querySelectorAll(".restore-trigger");
+    restoreArrows.forEach(async (restoreArrow) => {
+      if (restoreArrow === e.target) {
+        e.preventDefault();
+        const parentboard = restoreArrow.closest(".board");
+        parentboard.classList.add("goBack");
+        parentboard.addEventListener("animationend", () => {
+          DOMHandler.reload();
+        });
+        const id = parentboard.dataset.id;
+        STORE.restoringBoard(id)
+        await editBoard(id, {closed: false })
+      }
+    });
+  });
+}
 
 const ClosedPage = {
   toString() {
     return renderClosed();
   },
   addListeners() {
-    listenDelete()
+    listenDelete(), listenRestore()
   },
 };
 
